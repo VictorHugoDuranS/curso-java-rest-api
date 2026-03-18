5b641cd1bd57f8c7198473b8e8d7893b72b50f86
VictorHugoDuranSantiago <victor-duran31@hotmail.com>
2026-03-18 10:57:45 -0600

    Primer commit

---
+++ src/test/java/curso_java_rest_api/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package curso_java_rest_api.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
